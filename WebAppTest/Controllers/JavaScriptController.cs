33b9da5615dd2a7dca508783c693c0f783429903
Administrator <Administrator@00-10714>
2018-12-10 10:19:49 +0800

    加入專案檔案。

---
+++ WebAppTest/Controllers/JavaScriptController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebAppTest.Controllers
+{
+    [Authorize]
+    public class JavaScriptController : Controller
+    {
+
+        // GET: JavaScript
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Gojs前端圖形化插件()
+        {
+            return View();
+        }
+
+        public ActionResult Gojs自定義流程圖()
+        {
+            return View();
+        }
+
+        public ActionResult Gojs工業流程圖()
+        {
+            return View();
+        }
+    }
+}
---
+++ WebAppTest/Models/Product.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebAppTest.Models
+{
+    public class Product
+    {
+        /// <summary>  
+        /// Gets or sets the id.  
+        /// </summary>  
+        /// <value>The id.</value>  
+        [Key]
+        public int ProductId { get; set; }
+
+        /// <summary>  
+        /// 直接做型号处理  
+        /// </summary>  
+        public string ProductName { get; set; }
+        /// <summary>  
+        /// Gets or sets the category.  
+        /// </summary>  
+        /// <value>The category.</value>  
+        public string Category { get; set; }
+
+        /// <summary>  
+        /// Gets or sets the brand.  
+        /// </summary>  
+        /// <value>The brand.</value>  
+        public string Brand { get; set; }
+
+        public string Size { get; set; }
+
+        /// <summary>  
+        /// Gets or sets the 规格.  
+        /// </summary>  
+        /// <value>The standard.</value>  
+        public string Standard { get; set; }
+
+        /// <summary>  
+        /// 专柜  
+        /// </summary>  
+        public decimal ZhuanGui { get; set; }
+
+        /// <summary>  
+        /// Gets or sets the 莎莎 price.  
+        /// </summary>  
+        /// <value>The sa sa price.</value>  
+        public decimal SaSaPrice { get; set; }
+
+        /// <summary>  
+        /// Gets or sets the 卓越 price.  
+        /// </summary>  
+        /// <value>The zhuoyue price.</value>  
+        public decimal ZhuoyuePrice { get; set; }
+        /// <summary>  
+        /// Gets or sets the 卡莱美 price.  
+        /// </summary>  
+        /// <value>The ka lai price.</value>  
+        public decimal KaLaiPrice { get; set; }
+
+        /// <summary>  
+        /// 雅施  
+        /// </summary>  
+        public decimal YaShi { get; set; }
+
+        /// <summary>  
+        /// Gets or sets the 万宁 price.  
+        /// </summary>  
+        /// <value>The wanning price.</value>  
+        public decimal WanningPrice { get; set; }
+        /// <summary>  
+        /// Gets or sets the 屈臣氏 price.  
+        /// </summary>  
+        /// <value>The quchengshi price.</value>  
+        public decimal QuchengshiPrice { get; set; }
+        /// <summary>  
+        /// Gets or sets the 药店 price.  
+        /// </summary>  
+        /// <value>The drugstore price.</value>  
+        public decimal DrugstorePrice { get; set; }
+
+        /// <summary>  
+        /// Gets or sets the others.  
+        /// </summary>  
+        /// <value>The others.</value>  
+        public string Others { get; set; }
+
+        /// <summary>  
+        /// Gets or sets the create time.  
+        /// </summary>  
+        /// <value>The create time.</value>  
+        public DateTime CreateTime { get; set; }
+
+        public string ImgUrl { get; set; }
+
+        public Product()
+        {
+            CreateTime = DateTime.Now;
+        }
+    }
+}
